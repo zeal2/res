96174b33df27fd1e836d335b159d813d1cf99aa2
zeal <zealvala72@gmail.com>
2021-05-04 02:19:29 +0530

    added remote mysql connection details

--- firstimport.php
+++ firstimport.php
@@ -1,8 +1,15 @@
 <?php
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password=""; // Mysql password
-$db_name="railres"; // Database name
+//Development coonection
+//$host="localhost"; // Host name
+//$username="root"; // Mysql username
+//$password=""; // Mysql password
+//$db_name="railres"; // Database name
+
+//remote db connection
+$host="remotemysql.com"; // Host name
+$username="Kqye8K3zwa"; // Mysql username
+$password="LPCZ5qzYYG"; // Mysql password
+$db_name="Kqye8K3zwas"; // Database name
 
 $conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
 
